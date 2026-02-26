7ffc2f363f7683c922753424febe3d6479dfd642
Nikos.p <connor2000@hotmail.gr>
2026-02-26 04:05:16 +0200

    feat: add personal info relation

--- src/main/java/gr/aueb/cf/eduapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/eduapp/model/Teacher.java
@@ -39,6 +39,10 @@ public class Teacher extends AbstractEntity {
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "personal_info_id", nullable = false)
+    private PersonalInfo personalInfo;
+
     @PrePersist
     public void initializeUUID() {
         this.uuid = UUID.randomUUID();
